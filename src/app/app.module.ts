abd816efd9c6fe3174825ce9572cc13861137ea7
cpchung <chakpongchung@gmail.com>
2020-04-13 23:41:59 -0400

    to be deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -8,16 +9,22 @@ import {AppComponent} from './app.component';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
+import {CartComponent} from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
 
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
-      { path: 'products/:productId', component: ProductDetailsComponent },
+      {path: 'products/:productId', component: ProductDetailsComponent},
+      {path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent },
+
     ])
   ],
   declarations: [
@@ -25,7 +32,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     ProductAlertsComponent,
     TopBarComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
